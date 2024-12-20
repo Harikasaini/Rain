2d99bad2930bc2f1ca85897f11cb9243f3359dda
Harikasaini <164360453+Harikasaini@users.noreply.github.com>
2024-12-20 11:28:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function StartRain(){
+    let i=document.getElementById(img)
+    img.src="rain1.gif",width="600",height="200"
+}
+function StopRain(){
+    let i=document.getElementById(img)
+    img.src="rainbow.jpeg",width="600"
+}
